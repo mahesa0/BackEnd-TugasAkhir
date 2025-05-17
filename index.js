1011bfc2087fc15b1a91bca3efce45bbdf8ea7da
Thorik <axe.army194@gmail.com>
2025-05-17 16:06:00 +0700

    push all file

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import userRoutes from "./src/routes/UserRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+const PORT = process.env.PORT;
+app.use(bodyParser.json());
+
+// Routes
+app.use("/users", userRoutes);
+// app.use("/products", productRoutes);
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
