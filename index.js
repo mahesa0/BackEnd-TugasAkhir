b1b35b901a2c7ce8916a80b55d0e1274fb38e0b1
Thorik <axe.army194@gmail.com>
2025-05-17 21:51:50 +0700

    update CORS policy

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", ""],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 const PORT = process.env.PORT;
 app.use(bodyParser.json());
 
